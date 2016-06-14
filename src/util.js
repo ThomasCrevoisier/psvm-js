ca8aed0aaf7263ea8131e5dfbae426d1214629d6
ThomasCrvsr <crevoisier.thomas@gmail.com>
2016-06-14 23:16:13 +0200

    Add editorconfig (#19)

--- src/util.js
+++ src/util.js
@@ -5,107 +5,107 @@ var Promise = require('bluebird'),
     process = require('child_process'),
     rimraf = require('rimraf');
 
-function nuggetAsync (url, options) {
-  return new Promise(function (resolve, reject) {
-    nugget(url, options, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    })
-  });
-}
-
-function createNonExistingDir (path) {
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
-  }
-}
-
-function extract (src, dest) {
-  return new Promise(function (resolve, reject) {
-    console.log('Extracting ', src, ' into ', dest);
-    tarball.extractTarball(src, dest, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
+function nuggetAsync(url, options) {
+    return new Promise(function (resolve, reject) {
+        nugget(url, options, function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        })
     });
-  });
 }
 
-function rename (oldPath, newPath) {
-  return new Promise(function (resolve, reject) {
-    fs.rename(oldPath, newPath, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    })
-  });
+function createNonExistingDir(path) {
+    if (!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+    }
 }
 
-function parseResponseBody (res) {
-  return JSON.parse(res.body);
+function extract(src, dest) {
+    return new Promise(function (resolve, reject) {
+        console.log('Extracting ', src, ' into ', dest);
+        tarball.extractTarball(src, dest, function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
 }
 
-function copy (source, dest) {
-  return new Promise(function (resolve, reject) {
-    var readStream = fs.createReadStream(source),
-        writeStream = fs.createWriteStream(dest);
+function rename(oldPath, newPath) {
+    return new Promise(function (resolve, reject) {
+        fs.rename(oldPath, newPath, function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        })
+    });
+}
 
-    readStream.on('error', function (err) {
-      reject(err);
+function parseResponseBody(res) {
+    return JSON.parse(res.body);
+}
+
+function copy(source, dest) {
+    return new Promise(function (resolve, reject) {
+        var readStream = fs.createReadStream(source),
+            writeStream = fs.createWriteStream(dest);
+
+        readStream.on('error', function (err) {
+            reject(err);
+        });
+
+        writeStream.on('error', function (err) {
+            reject(err);
+        });
+
+        writeStream.on('close', function () {
+            resolve();
+        });
+
+        readStream.pipe(writeStream);
+    });
+}
+
+function command(cmd) {
+    var promise = new Promise(function (resolve, reject) {
+        process.exec(cmd, function (error, stdout, stderr) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(stdout);
+            }
+        });
     });
 
-    writeStream.on('error', function (err) {
-      reject(err);
-    });
-
-    writeStream.on('close', function () {
-      resolve();
-    });
-
-    readStream.pipe(writeStream);
-  });
+    return promise;
 }
 
-function command (cmd) {
-  var promise = new Promise(function (resolve, reject) {
-    process.exec(cmd, function (error, stdout, stderr) {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(stdout);
-      }
+function deleteDir(path) {
+    return new Promise(function (resolve, reject) {
+        rimraf(path, function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        })
     });
-  });
-
-  return promise;
-}
-
-function deleteDir (path) {
-  return new Promise (function (resolve, reject) {
-    rimraf(path, function (err) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
-      }
-    })
-  });
 }
 
 module.exports = {
-  nuggetAsync: nuggetAsync,
-  createNonExistingDir: createNonExistingDir,
-  extract: extract,
-  rename: rename,
-  parseResponseBody: parseResponseBody,
-  copy: copy,
-  command: command,
-  deleteDir: deleteDir
+    nuggetAsync: nuggetAsync,
+    createNonExistingDir: createNonExistingDir,
+    extract: extract,
+    rename: rename,
+    parseResponseBody: parseResponseBody,
+    copy: copy,
+    command: command,
+    deleteDir: deleteDir
 }
